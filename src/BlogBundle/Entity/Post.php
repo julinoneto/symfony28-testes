2218735ea3342dcdb2389f607ec13c8595a2c3b0
Julino Neto <julinotds@gmail.com>
2016-02-28 19:41:47 -0400

    Instala o Bower e as principais dependencias (jquery, bootstrap, fontawesome)

---
+++ src/BlogBundle/Entity/Post.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace BlogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Post
+ *
+ * @ORM\Table(name="post")
+ * @ORM\Entity(repositoryClass="BlogBundle\Repository\PostRepository")
+ */
+class Post
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titulo", type="string", length=255)
+     */
+    private $titulo;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="conteudo", type="text")
+     */
+    private $conteudo;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titulo
+     *
+     * @param string $titulo
+     * @return Post
+     */
+    public function setTitulo($titulo)
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    /**
+     * Get titulo
+     *
+     * @return string 
+     */
+    public function getTitulo()
+    {
+        return $this->titulo;
+    }
+
+    /**
+     * Set conteudo
+     *
+     * @param string $conteudo
+     * @return Post
+     */
+    public function setConteudo($conteudo)
+    {
+        $this->conteudo = $conteudo;
+
+        return $this;
+    }
+
+    /**
+     * Get conteudo
+     *
+     * @return string 
+     */
+    public function getConteudo()
+    {
+        return $this->conteudo;
+    }
+}
